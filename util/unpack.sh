a6695e337c860b7462e1a4a74230f30a4aa40478
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-07-27 12:49:20 +0200

    Fix of unpack, added <PKG>_ORIG variable
    - <PKG>_ORIG defines name of directory inside archive,
      the script finally performs:
        mv <PKG>_ORIG <PKG>_DIR

--- util/unpack.sh
+++ util/unpack.sh
@@ -1,32 +1,68 @@
 #! /bin/sh
 
-archive_dir="$1"
-shift
-archive_name="$1"
-shift
-archive_fmt="$1"
-shift
-archive_dst="$1"
-archive_dstdir="`dirname $archive_dst`"
-shift
+unpack_tmp()
+{
+    archive_dir="$1"
+    shift
+    archive_name="$1"
+    shift
+    archive_fmt="$1"
+    shift
+    
+    archive_path="$archive_dir/$archive_name.$archive_fmt"
 
-archive_path="$archive_dir/$archive_name.$archive_fmt"
-archive_target="$archive_dstdir"
+    case "$archive_fmt" in
+        "tar.bz2" | "tar.gz")
+            echo "[UNPACK] Processing $archive_name" > /dev/stderr
+            tar "$@" -xf "$archive_path" -C "$archive_dir"
+        ;;
 
-if [ -d "$archive_dst" ]; then
-    echo "[UNPACK] Skipping $archive_name" > /dev/stderr
-    exit
+        *)
+            echo "[UNPACK] Unsupported format of archive: $archive_fmt" > /dev/stderr
+            exit 1
+        ;;
+    esac
+}
+
+move_tmp()
+{
+    archive_dst="$1"
+    shift
+    archive_orig="$1"
+    shift
+    archive_dir="$1"
+    shift
+
+    mkdir -p "`dirname $archive_dst`"
+    mv "$archive_dir/$archive_orig" "$archive_dst"
+}
+
+unpack_and_move()
+{
+    archive_dir="$1"
+    shift
+    archive_name="$1"
+    shift
+    archive_fmt="$1"
+    shift
+    archive_dst="$1"
+    shift
+    archive_orig="$1"
+    shift
+
+    if [ -d "$archive_dst" ]; then
+        echo "[UNPACK] Skipping $archive_name" > /dev/stderr
+        exit
+    fi
+
+    unpack_tmp "$archive_dir" "$archive_name" "$archive_fmt" "$@"
+    move_tmp "$archive_dst" "$archive_orig" "$archive_dir"
+}
+
+if [ "$1" = "--tmp" ]; then
+    shift
+    unpack_tmp "$@"
+else
+    unpack_and_move "$@"
 fi
-
-case "$archive_fmt" in
-    "tar.bz2" | "tar.gz")
-        echo "[UNPACK] Processing $archive_name" > /dev/stderr
-        tar "$@" -xf "$archive_path" -C "$archive_target"
-    ;;
-
-    *)
-        echo "[UNPACK] Unsupported format of archive: $archive_fmt" > /dev/stderr
-        exit 1
-    ;;
-esac
 
