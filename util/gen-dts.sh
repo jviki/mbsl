6977d9f9d89069010a6d7eb4ee1343465acaf125
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-07-29 10:37:37 +0200

    Script gen-dts.sh to generate DTS file from MSS and MHS

---
+++ util/gen-dts.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+dts_generate()
+{
+	tmp_dir="$1"
+	shift
+	bsp_dir="$1"
+	shift
+	processor="$1"
+	shift
+	mhs_file="$1"
+	shift
+	mss_file="$1"
+	shift
+	fpga_type="$1"
+	shift
+	dts_src="$1"
+	shift
+	dts_dst="$1"
+	shift
+
+	echo "[DTS-GEN] Generating DTS file ($fpga_type, $processor, $bsp_dir)" > /dev/stderr
+
+	check=`grep 'OS[ \t]*standalone' $mss_file | wc -l`
+	if [ "$check" != "0" ]; then
+		echo "[DTS-GEN] The MSS file $mss_file seems to be incorrect (OS standalone)" > /dev/stderr
+	fi
+
+	mkdir -p "$tmp_dir"
+	libgen -od "$tmp_dir" -lp "$bsp_dir" -mhs "$mhs_file" -p "$fpga_type" -pe "$processor" "$mss_file"
+	
+	if [ "$?" != "0" ]; then
+		exit 1
+	fi
+
+	cp "$dts_src" "$dts_dst"
+#	rm -Rf "$tmp_dir"
+	ls -lh "$dts_dst"
+}
+
+dts_generate "$@"
