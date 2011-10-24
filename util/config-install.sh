5dd53ff4b16e6612566d97a8df2f2e41fc9c8e3b
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-10-24 23:20:40 +0200

    Description of config-install.sh utility

--- util/config-install.sh
+++ util/config-install.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 
+###
+# Installs selected kernel/busybox/other config file to used
+# by the build system. Target config file name is .config to be
+# compatible with Linux kernel and Busybox build systems. When
+# there already exists one, it is backed up (with extension: bak).
+#
+# All logging is prefixed by "[CONFIG-INSTALL]" string.
+#
+# Usage: ./config-install <path-to-file.config> <path-to-dest-dir>
+###
+
 config_src="$1"
 shift
 config_dst="$1"
